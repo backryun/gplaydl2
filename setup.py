315859945da7d1c8cb8d689e9111de59de647d25
backryun <bakryun0718@gmail.com>
2023-04-17 19:15:09 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
 setup(name='gplaydl',
-	version='1.3.5',
+	version='1.4.0',
 	description='Google Play APK downloader command line utility that utilizes gpapi to download APK files of free apps and games.',
-	author="Rehmat Alam",
-	author_email="contact@rehmat.works",
-	url="https://github.com/rehmatworks/gplaydl",
+	author="Backryun",
+	author_email="bakryun0718@gmail.com",
+	url="https://github.com/backryun/gplaydl2",
 	python_requires='>3.2.0',
 	license="MIT",
 	entry_points={
@@ -17,19 +17,19 @@ setup(name='gplaydl',
 		'gplaydl'
 	],
 	install_requires=[
-		'certifi==2020.4.5.1',
-		'cffi==1.14.0',
-		'chardet==3.0.4',
-		'cryptography==2.9',
+		'certifi==2022.12.7',
+		'cffi==1.15.1',
+		'chardet==5.1.0',
+		'cryptography==3.4.8',
 		'decorator==4.4.2',
-		'gpapidl==1.0.2',
-		'idna==2.9',
+		'gpapi==1.0.0',
+		'idna==2.10',
 		'protobuf==3.14.0',
-		'pycparser==2.20',
-		'requests==2.23.0',
+		'pycparser==2.21',
+		'requests==2.28.2',
 		'six==1.14.0',
 		'termcolor==1.1.0',
-		'urllib3==1.25.8',
+		'urllib3==1.25.11',
 		'validators==0.14.3'
 	]
 )
